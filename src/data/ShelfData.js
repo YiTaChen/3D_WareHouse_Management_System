36d85d32c78fa8565a0100d303a3a8ed783540ee
Yi-Ta <f8162426@hotmail.com>
2025-06-24 12:13:35 -0700

    feat: refactor ShelfData and layoutData from simple use to approach realistic usage layout

--- src/data/ShelfData.js
+++ src/data/ShelfData.js
@@ -1,20 +1,88 @@
 
-export const ShelfData ={
-  shelves: [
-    { id: 'shelf001', position: [-8, 0, -8], rotation: [0, 0, 0] },
-    { id: 'shelf0001', position: [-8, 2, -8], rotation: [0, 0, 0] },
-    { id: 'shelf002', position: [-8, 0,  -6], rotation: [0, 0, 0] },
-    { id: 'shelf003', position: [-8, 0, -4], rotation: [0, 0, 0] },
-    { id: 'shelf004', position: [-8, 0, -2], rotation: [0, 0, 0] },
-    { id: 'shelf005', position: [-8, 0, 0], rotation: [0, 0, 0] },
-    { id: 'shelf006', position: [-8, 0, 2], rotation: [0, 0, 0] },
-    { id: 'shelf007', position: [-8, 0, 4], rotation: [0, 0, 0] },
-    { id: 'shelf008', position: [-8, 0, 6], rotation: [0, 0, 0] },
-    { id: 'shelf009', position: [-8, 0, 8], rotation: [0, 0, 0] },
+// export const ShelfData ={
+//   shelves: [
+
+//     { id: 'shelf001', position: [-0, 0, -0], rotation: [0, 0, 0] },
+    
+//     // { id: 'shelf001', position: [-8, 0, -8], rotation: [0, 0, 0] },
+//     // { id: 'shelf0001', position: [-8, 2, -8], rotation: [0, 0, 0] },
+//     // { id: 'shelf002', position: [-8, 0,  -6], rotation: [0, 0, 0] },
+//     // { id: 'shelf003', position: [-8, 0, -4], rotation: [0, 0, 0] },
+//     // { id: 'shelf004', position: [-8, 0, -2], rotation: [0, 0, 0] },
+//     // { id: 'shelf005', position: [-8, 0, 0], rotation: [0, 0, 0] },
+//     // { id: 'shelf006', position: [-8, 0, 2], rotation: [0, 0, 0] },
+//     // { id: 'shelf007', position: [-8, 0, 4], rotation: [0, 0, 0] },
+//     // { id: 'shelf008', position: [-8, 0, 6], rotation: [0, 0, 0] },
+//     // { id: 'shelf009', position: [-8, 0, 8], rotation: [0, 0, 0] },
  
  
   
-  ]
+//   ]
+// }
+
+// export default ShelfData;
+
+
+
+
+// const shelves = [];
+
+// let count = 0;
+
+
+
+// for (let z = -8; z <= 8; z += 2) {
+//   const zPassNumber = new Set([-6, 0, 6, 8]);
+//   if (zPassNumber.has(z)) continue; // 跳過 z = 0 的層
+
+
+//   for (let j = 0; j < 5; j++) {
+//       for (let i = 0; i < 20; i++) {
+//         shelves.push({
+//           id: `shelf${String(count + 5).padStart(3, '0')}`, // shelf005 ~ shelf009
+//           position: [i*2 + 2, ( 2 * j ) , z ],
+//           rotation: [0, 0, 0],
+//         });
+//         count++;
+//       }
+//   }
+// }
+
+// export const ShelfData = {
+//   shelves,
+// };
+
+// export default ShelfData;
+
+
+
+
+
+const shelves = [];
+
+let count = 0;
+
+
+
+for (let z = -8; z <= 8; z += 2) {
+  const zPassNumber = new Set([-6, 0, 6, 8]);
+  if (zPassNumber.has(z)) continue; // 跳過 z = 0 的層
+
+
+  for (let j = 0; j < 4; j++) {
+      for (let i = 0; i < 10; i++) {
+        shelves.push({
+          id: `shelf${String(count + 5).padStart(3, '0')}`, // shelf005 ~ shelf009
+          position: [i*2 + 2, ( 2 * j ) , z ],
+          rotation: [0, 0, 0],
+        });
+        count++;
+      }
+  }
 }
+
+export const ShelfData = {
+  shelves,
+};
 
 export default ShelfData;
--- src/data/layoutData.js
+++ src/data/layoutData.js
@@ -1,33 +1,88 @@
 // src/data/layoutData.js
+// export const layoutData ={
+//   conveyors: [
+//     // { id: 'conv1', position: [-4, 0, 4], rotation: [0, Math.PI /2, 0] },
+//     // { id: 'conv5', position: [6, 0, 6], rotation: [0, 0, 0] },
+//     { id: 'conv2', position: [0, 0, 0], rotation: [0, 0, 0] },
+//     { id: 'conv3', position: [2, 0, 0], rotation: [0, 0, 0] },
+//     { id: 'conv4', position: [4, 0, 0], rotation: [0, 0, 0] },
+//     { id: 'conv5', position: [6, 0, 0], rotation: [0, 0, 0] },
+//     { id: 'conv6', position: [8, 0, 0], rotation: [0, 0, 0] },
+//     { id: 'conv7', position: [10, 0, 0], rotation: [0, -Math.PI /2, 0] },
+//     { id: 'conv8', position: [10, 0, 2], rotation: [0, -Math.PI /2, 0] },
+//     { id: 'conv9', position: [10, 0, 4], rotation: [0, -Math.PI /2, 0] },
+//     { id: 'conv10', position: [10, 0, 6], rotation: [0, -Math.PI /2, 0] },
+//     { id: 'conv11', position: [10, 0, 8], rotation: [0, -Math.PI , 0] },
+//     { id: 'conv12', position: [8, 0, 8], rotation: [0, -Math.PI , 0] },
+//     { id: 'conv13', position: [6, 0, 8], rotation: [0, -Math.PI, 0] },
+//     { id: 'conv14', position: [4, 0, 8], rotation: [0, -Math.PI, 0] },
+//     { id: 'conv15', position: [2, 0, 8], rotation: [0, -Math.PI, 0] },
+//     { id: 'conv16', position: [0, 0, 8], rotation: [0, -Math.PI, 0] },
+//     { id: 'conv17', position: [-2, 0, 8], rotation: [0, -Math.PI , 0] },
+//     { id: 'conv18', position: [-4, 0, 8], rotation: [0, Math.PI /2 , 0] },
+//     { id: 'conv19', position: [-4, 0, 6], rotation: [0, Math.PI /2 , 0] },
+//     { id: 'conv20', position: [-4, 0, 4], rotation: [0, Math.PI /2 , 0] },
+//     { id: 'conv21', position: [-4, 0, 2], rotation: [0, Math.PI /2 , 0] },
+//     { id: 'conv22', position: [-4, 0, 0], rotation: [0, Math.PI /2 , 0] },
+//     { id: 'conv23', position: [-4, 0, -2], rotation: [0, Math.PI /2 , 0] },
+//     { id: 'conv24', position: [-4, 0, -4], rotation: [0, Math.PI /2 , 0] },
+  
+//   ]
+// }
+
+const r90 = - Math.PI / 2;
+const r180 = -Math.PI /2;
+const r270 = Math.PI /2;
+
 export const layoutData ={
+
+  
+
   conveyors: [
-    { id: 'conv1', position: [-4, 0, 4], rotation: [0, Math.PI /2, 0] },
-    // { id: 'conv5', position: [6, 0, 6], rotation: [0, 0, 0] },
-    { id: 'conv2', position: [0, 0, 0], rotation: [0, 0, 0] },
-    { id: 'conv3', position: [2, 0, 0], rotation: [0, 0, 0] },
-    { id: 'conv4', position: [4, 0, 0], rotation: [0, 0, 0] },
-    { id: 'conv5', position: [6, 0, 0], rotation: [0, 0, 0] },
-    { id: 'conv6', position: [8, 0, 0], rotation: [0, 0, 0] },
-    { id: 'conv7', position: [10, 0, 0], rotation: [0, -Math.PI /2, 0] },
-    { id: 'conv8', position: [10, 0, 2], rotation: [0, -Math.PI /2, 0] },
-    { id: 'conv9', position: [10, 0, 4], rotation: [0, -Math.PI /2, 0] },
-    { id: 'conv10', position: [10, 0, 6], rotation: [0, -Math.PI /2, 0] },
-    { id: 'conv11', position: [10, 0, 8], rotation: [0, -Math.PI , 0] },
-    { id: 'conv12', position: [8, 0, 8], rotation: [0, -Math.PI , 0] },
-    { id: 'conv13', position: [6, 0, 8], rotation: [0, -Math.PI, 0] },
-    { id: 'conv14', position: [4, 0, 8], rotation: [0, -Math.PI, 0] },
-    { id: 'conv15', position: [2, 0, 8], rotation: [0, -Math.PI, 0] },
-    { id: 'conv16', position: [0, 0, 8], rotation: [0, -Math.PI, 0] },
-    { id: 'conv17', position: [-2, 0, 8], rotation: [0, -Math.PI , 0] },
-    { id: 'conv18', position: [-4, 0, 8], rotation: [0, Math.PI /2 , 0] },
-    { id: 'conv19', position: [-4, 0, 6], rotation: [0, Math.PI /2 , 0] },
-    { id: 'conv20', position: [-4, 0, 4], rotation: [0, Math.PI /2 , 0] },
-    { id: 'conv21', position: [-4, 0, 2], rotation: [0, Math.PI /2 , 0] },
-    { id: 'conv22', position: [-4, 0, 0], rotation: [0, Math.PI /2 , 0] },
-    { id: 'conv23', position: [-4, 0, -2], rotation: [0, Math.PI /2 , 0] },
-    { id: 'conv24', position: [-4, 0, -4], rotation: [0, Math.PI /2 , 0] },
+
+    { id: 'conv1', position: [-8, 0, -8], rotation: [0, 0, 0] },
+    { id: 'conv2', position: [-6, 0, -8], rotation: [0, 0, 0] },
+    { id: 'conv3', position: [-4, 0, -8], rotation: [0, 0, 0] },
+    { id: 'conv4', position: [-8, 0, -4], rotation: [0, 0, 0] },
+    { id: 'conv5', position: [-6, 0, -4], rotation: [0, 0, 0] },
+    { id: 'conv6', position: [-4, 0, -4], rotation: [0, 0, 0] },
+    
+    { id: 'conv7', position: [-8, 0, 2], rotation: [0, 0, 0] },
+    { id: 'conv8', position: [-6, 0, 2], rotation: [0, 0, 0] },
+    { id: 'conv9', position: [-4, 0, 2], rotation: [0, 0, 0] },
+
+    { id: 'conv10', position: [-8, 0, 8], rotation: [0, 0, 0] },
+    { id: 'conv11', position: [-6, 0, 8], rotation: [0, 0, 0] },
+    { id: 'conv12', position: [-4, 0, 8], rotation: [0, 0, 0] },
+
+
+    { id: 'conv13', position: [-4, 2, 8], rotation: [0, 0, 0] },
+    { id: 'conv14', position: [-6, 2, 8], rotation: [0, r270, 0] },
+    { id: 'conv16', position: [-6, 1.67, 9.1], rotation: [ 0.18 * Math.PI, r270, 0] },
+    { id: 'conv17', position: [-6, 0.71, 10.6], rotation: [ 0.18 * Math.PI, r270, 0] },
+    
+    // { id: 'conv17', position: [-6, 0.71, 10.6], rotation: [ 0, r270,  1/2* Math.PI] },
+    
+    { id: 'conv18', position: [-6, 0, 13.0], rotation: [ 0, r270, 0] },
+    { id: 'conv19', position: [-6, 0, 15.0], rotation: [ 0, r270, 0] },
+    // { id: 'conv15', position: [-6, 1.75, 9.6], rotation: [ 0.1 * Math.PI, r270, 0] },
+
+
+    // { id: 'conv13', position: [6, 0, 8], rotation: [0, -Math.PI, 0] },
+    // { id: 'conv14', position: [4, 0, 8], rotation: [0, -Math.PI, 0] },
+    // { id: 'conv15', position: [2, 0, 8], rotation: [0, -Math.PI, 0] },
+    // { id: 'conv16', position: [0, 0, 8], rotation: [0, -Math.PI, 0] },
+    // { id: 'conv17', position: [-2, 0, 8], rotation: [0, -Math.PI , 0] },
+    // { id: 'conv18', position: [-4, 0, 8], rotation: [0, Math.PI /2 , 0] },
+    // { id: 'conv19', position: [-4, 0, 6], rotation: [0, Math.PI /2 , 0] },
+    // { id: 'conv20', position: [-4, 0, 4], rotation: [0, Math.PI /2 , 0] },
+    // { id: 'conv21', position: [-4, 0, 2], rotation: [0, Math.PI /2 , 0] },
+    // { id: 'conv22', position: [-4, 0, 0], rotation: [0, Math.PI /2 , 0] },
+    // { id: 'conv23', position: [-4, 0, -2], rotation: [0, Math.PI /2 , 0] },
+    // { id: 'conv24', position: [-4, 0, -4], rotation: [0, Math.PI /2 , 0] },
   
   ]
 }
+
 
 export default layoutData;
