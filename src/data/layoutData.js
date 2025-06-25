63aedae742abd4a5a1565aff3e0e51ef4c133ef3
Yi-Ta <f8162426@hotmail.com>
2025-06-24 22:04:25 -0700

    feat: add type property to conveyors for better classification and update layoutData export

--- src/data/layoutData.js
+++ src/data/layoutData.js
@@ -40,49 +40,37 @@ export const layoutData ={
 
   conveyors: [
 
-    { id: 'conv1', position: [-8, 0, -8], rotation: [0, 0, 0] },
-    { id: 'conv2', position: [-6, 0, -8], rotation: [0, 0, 0] },
-    { id: 'conv3', position: [-4, 0, -8], rotation: [0, 0, 0] },
-    { id: 'conv4', position: [-8, 0, -4], rotation: [0, 0, 0] },
-    { id: 'conv5', position: [-6, 0, -4], rotation: [0, 0, 0] },
-    { id: 'conv6', position: [-4, 0, -4], rotation: [0, 0, 0] },
+    { id: 'conv1', position: [-8, 0, -8], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv2', position: [-6, 0, -8], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv3', position: [-4, 0, -8], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv4', position: [-8, 0, -4], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv5', position: [-6, 0, -4], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv6', position: [-4, 0, -4], rotation: [0, 0, 0] , type: 'straight'},
     
-    { id: 'conv7', position: [-8, 0, 2], rotation: [0, 0, 0] },
-    { id: 'conv8', position: [-6, 0, 2], rotation: [0, 0, 0] },
-    { id: 'conv9', position: [-4, 0, 2], rotation: [0, 0, 0] },
+    { id: 'conv7', position: [-8, 0, 2], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv8', position: [-6, 0, 2], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv9', position: [-4, 0, 2], rotation: [0, 0, 0] , type: 'straight'},
 
-    { id: 'conv10', position: [-8, 0, 8], rotation: [0, 0, 0] },
-    { id: 'conv11', position: [-6, 0, 8], rotation: [0, 0, 0] },
-    { id: 'conv12', position: [-4, 0, 8], rotation: [0, 0, 0] },
+    { id: 'conv10', position: [-8, 0, 8], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv11', position: [-6, 0, 8], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv12', position: [-4, 0, 8], rotation: [0, 0, 0] , type: 'straight'},
 
 
-    { id: 'conv13', position: [-4, 2, 8], rotation: [0, 0, 0] },
-    { id: 'conv14', position: [-6, 2, 8], rotation: [0, r270, 0] },
-    { id: 'conv16', position: [-6, 1.67, 9.1], rotation: [ 0.18 * Math.PI, r270, 0] },
-    { id: 'conv17', position: [-6, 0.71, 10.6], rotation: [ 0.18 * Math.PI, r270, 0] },
+    { id: 'conv13', position: [-4, 2, 8], rotation: [0, 0, 0] , type: 'straight'},
+    { id: 'conv14', position: [-6, 2, 8], rotation: [0, r270, 0], type: 'turn' },
+    { id: 'conv16', position: [-6, 1.67, 9.1], rotation: [ 0.18 * Math.PI, r270, 0] , type: 'slope'},
+    { id: 'conv17', position: [-6, 0.71, 10.6], rotation: [ 0.18 * Math.PI, r270, 0], type: 'slope' },
     
     // { id: 'conv17', position: [-6, 0.71, 10.6], rotation: [ 0, r270,  1/2* Math.PI] },
     
-    { id: 'conv18', position: [-6, 0, 13.0], rotation: [ 0, r270, 0] },
-    { id: 'conv19', position: [-6, 0, 15.0], rotation: [ 0, r270, 0] },
-    // { id: 'conv15', position: [-6, 1.75, 9.6], rotation: [ 0.1 * Math.PI, r270, 0] },
-
-
-    // { id: 'conv13', position: [6, 0, 8], rotation: [0, -Math.PI, 0] },
-    // { id: 'conv14', position: [4, 0, 8], rotation: [0, -Math.PI, 0] },
-    // { id: 'conv15', position: [2, 0, 8], rotation: [0, -Math.PI, 0] },
-    // { id: 'conv16', position: [0, 0, 8], rotation: [0, -Math.PI, 0] },
-    // { id: 'conv17', position: [-2, 0, 8], rotation: [0, -Math.PI , 0] },
-    // { id: 'conv18', position: [-4, 0, 8], rotation: [0, Math.PI /2 , 0] },
-    // { id: 'conv19', position: [-4, 0, 6], rotation: [0, Math.PI /2 , 0] },
-    // { id: 'conv20', position: [-4, 0, 4], rotation: [0, Math.PI /2 , 0] },
-    // { id: 'conv21', position: [-4, 0, 2], rotation: [0, Math.PI /2 , 0] },
-    // { id: 'conv22', position: [-4, 0, 0], rotation: [0, Math.PI /2 , 0] },
-    // { id: 'conv23', position: [-4, 0, -2], rotation: [0, Math.PI /2 , 0] },
-    // { id: 'conv24', position: [-4, 0, -4], rotation: [0, Math.PI /2 , 0] },
-  
+    { id: 'conv18', position: [-6, 0, 13.0], rotation: [ 0, r270, 0] , type: 'straight'},
+    { id: 'conv19', position: [-6, 0, 15.0], rotation: [ 0, r270, 0] , type: 'straight'},
+    
+ 
   ]
 }
+export const ConveyorData = layoutData
 
 
 export default layoutData;
+
